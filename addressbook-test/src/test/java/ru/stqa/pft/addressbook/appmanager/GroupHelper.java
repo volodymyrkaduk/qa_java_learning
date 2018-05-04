ad7d1cdfef98b70734b2345b62f77184f956eda0
volodymyrkaduk <vladimir.kaduk@gmail.com>
2018-05-04 13:05:56 +0200

    complete task 4

---
+++ addressbook-test/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -0,0 +1,43 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+/**
+ * Created by vkaduk on 04.05.18.
+ */
+public class GroupHelper extends HelperBase {
+
+    public GroupHelper(FirefoxDriver wd) {
+        super(wd);
+    }
+
+    public void returnToGroupPage() {
+        click(By.linkText("group page"));
+    }
+
+    public void submitGroupCreation() {
+        click(By.name("submit"));
+    }
+
+    public void fillGroupForm(GroupData groupData) {
+        type(By.name("group_name"), groupData.getName());
+        type(By.name("group_header"), groupData.getHeader());
+        type(By.name("group_footer"), groupData.getFooter());
+    }
+
+    public void initGroupCreation() {
+        click(By.name("new"));
+    }
+
+    public void deleteSelectedGroup() {
+        click(By.name("delete"));
+    }
+
+    public void selectGroup() {
+        if (!wd.findElement(By.name("selected[]")).isSelected()) {
+            click(By.name("selected[]"));
+        }
+    }
+}
---
+++ addressbook-test/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by vkaduk on 04.05.18.
+ */
+public class NavigationHelper extends HelperBase {
+
+    public NavigationHelper(FirefoxDriver wd) {
+        super(wd);
+    }
+
+    public void goToGroupPage() {
+        click(By.linkText("groups"));
+    }
+}
